77b3e4406d96fb606c503e77634af8ae6acdf498
Marcos Ariel Popovich <59690249+AssusHawker@users.noreply.github.com>
2021-12-17 21:12:19 -0300

    Create Fetch.js

---
+++ Fetch.js
@@ -0,0 +1,19 @@
+//Fetch que trae una imagen al azar de la web de giphy y la muestra en pantalla 
+const apiKey = '4FyY08380xDweUCeLPCSxz8J6Wc0HyRX';
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+peticion
+  .then(resp =>resp.json())
+  .then(({ data }) =>{ 
+  //console.log(data.images.original.url)
+  const { url } = data.images.original;
+  const img = document.createElement('img');
+  img.src = url;
+
+  document.body.append( img );
+
+
+})
+
+.catch( console.warn )
